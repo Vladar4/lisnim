049b329d06c42078892b1176d623dca7397c008c
Vladar4 <vladar4@gmail.com>
2016-04-14 18:28:04 +0300

    added license text

--- lis.nim
+++ lis.nim
@@ -1,3 +1,26 @@
+# lis.nim
+# Copyright (C) 2016 Vladar
+# 
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# 
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+# 
+# Vladar vladar4@gmail.com
+
+
+
 import
   os, parseutils, rationals, strutils, tables
 
@@ -454,7 +477,7 @@ proc eval(x: Atom, env: Env = global_env): Atom =
             return atom(false)
           else:
             env[name.s] = eval(cdr[1], env)
-        return atom(true)
+            return env[name.s]
 
       elif car.s == "defun": # (defun name (args...) body)
         let cdr = x.cdr.list
